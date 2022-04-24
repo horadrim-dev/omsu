027ecb8f63db4eb959254fc889d1deadd4f74e47
horadrim-dev <email.alexss@gmail.com>
2022-04-24 18:34:53 +0700

    added bootstrap menu

--- app/app/static/app/js/custom.js
+++ app/app/static/app/js/custom.js
@@ -1,7 +1,7 @@
 //https://dimsemenov.com/plugins/magnific-popup/
-$(document).on('click tap', '#content img', function(e) {
-     // Already wrapped a fancybox href around the clicked image
-    /*
+$(document).on('click tap', '#content img', function (e) {
+	// Already wrapped a fancybox href around the clicked image
+	/*
      if ($(this).parent().is('a')) {
          return false;
      }
@@ -28,31 +28,46 @@ $(document).on('click tap', '#content img', function(e) {
 });
 
 
+document.addEventListener('click', function (e) {
+	// Hamburger menu
+	if (e.target.classList.contains('hamburger-toggle')) {
+		e.target.children[0].classList.toggle('active');
+	}
+})
 
+$(document).ready(function () {
+	$('#content img').wrap(function () {
+			return '<a href="' + this.src + '" class="zoom-wrapper" title="' + this.alt + '" />';
+		})
+		.parent()
+		.magnificPopup({
+			type: 'image',
+			closeOnContentClick: true,
+			closeBtnInside: false,
+			fixedContentPos: true,
+			mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+			image: {
+				verticalFit: true
+			},
+			zoom: {
+				enabled: true,
+				duration: 300 // don't foget to change the duration also in CSS
+			}
+		})
 
-$(document).ready(function() {
-	$('#content img').wrap(function() {
-        return '<a href="' + this.src + '" class="zoom-wrapper" title="' + this.alt + '" />';
-    })
-    .parent()
-    .magnificPopup({
-		type: 'image',
-		closeOnContentClick: true,
-		closeBtnInside: false,
-		fixedContentPos: true,
-		mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
-		image: {
-			verticalFit: true
-		},
-		zoom: {
-			enabled: true,
-			duration: 300 // don't foget to change the duration also in CSS
-		}
-	})
-
-	$('#back').click(function (){
+	// кнопка "назад"
+	$('#back').click(function () {
 		history.back();
 	});
+
+	// На больших экранах заставляем dropdown выпадать по hover
+	if ($(window).width() > '1000') {
+		$('.dropdown').hover(function () {
+			$(this).find('.dropdown-menu').first().addClass('show');
+		}, function () {
+			$(this).find('.dropdown-menu').first().removeClass('show');
+		});
+	};
 });
 /*
 	$('#content').magnificPopup({
