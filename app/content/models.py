a00509b9fd155533580057c8ed2a221770cd5199
horadrim-dev <email.alexss@gmail.com>
2022-05-19 17:27:54 +0700

    started abstracting content

--- app/content/models.py
+++ app/content/models.py
@@ -6,18 +6,12 @@ from ckeditor.fields import RichTextField
 from app.utils import slugify_rus
 
 # Create your models here.
+class Content(models.Model):
 
-class Feed(models.Model):
 
-    menu = models.ManyToManyField(Menu, verbose_name="Привязка к меню")
-
-    title = models.CharField(max_length=1000, verbose_name="Заголовок")
-    alias = models.SlugField(default="", blank=True, unique=True,
+    title = models.CharField(default="", max_length=1000, verbose_name="Заголовок")
+    alias = models.SlugField(default="", blank=True, 
                              max_length=1000, help_text="Краткое название транслитом через тире (пример: 'kratkoe-nazvanie-translitom'). Чем короче тем лучше. Для автоматического заполнения - оставьте пустым.")
-    description = RichTextUploadingField()
-
-    def __str__(self):
-        return self.title
 
     def save(self, lock_recursion=False, *args, **kwargs):
         # только при создании объекта, id еще не существует
@@ -29,27 +23,25 @@ class Feed(models.Model):
         super(Feed, self).save(*args, **kwargs)
 
 
-class Post(models.Model):
+    def __str__(self):
+        return self.title
 
-    title = models.CharField(max_length=1000, verbose_name="Заголовок")
-    alias = models.SlugField(default="", blank=True, 
-                             max_length=1000, help_text="Краткое название транслитом через тире (пример: 'kratkoe-nazvanie-translitom'). Чем короче тем лучше. Для автоматического заполнения - оставьте пустым.")
+    class Meta:
+        abstract = True
+
+
+class Feed(Content):
+
+    menu = models.ManyToManyField(Menu, verbose_name="Привязка к меню")
+    # alias = models.SlugField(default="", blank=True, unique=True,
+                            #  max_length=1000, help_text="Краткое название транслитом через тире (пример: 'kratkoe-nazvanie-translitom'). Чем короче тем лучше. Для автоматического заполнения - оставьте пустым.")
+    description = RichTextUploadingField()
+
+
+class Post(Content):
+
+    # title = models.CharField(max_length=1000, verbose_name="Заголовок")
     menu = models.ForeignKey(
         Menu, on_delete=models.CASCADE, verbose_name="Привязка к меню", blank=True, null=True)
     feed = models.ManyToManyField(Feed, blank=True, verbose_name="Категория")
     text = RichTextUploadingField()
-
-    def __str__(self):
-        return self.title
-
-    def save(self, lock_recursion=False, *args, **kwargs):
-        # только при создании объекта, id еще не существует
-        if not self.id:
-            if not self.alias:
-                # заполняем алиас
-                self.alias = slugify_rus(self.title)
-
-        # if not self.menu_id and not self.feed:
-        #     raise Exception('OMG!')
-
-        super(Post, self).save(*args, **kwargs)
