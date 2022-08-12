1e0159e0d9f5de843b868578594c8f7d72b4ce3e
horadrim-dev <email.alexss@gmail.com>
2022-08-12 11:37:12 +0700

    add filter toggle

--- app/app/static/app/js/custom.js
+++ app/app/static/app/js/custom.js
@@ -143,6 +143,13 @@ $(document).ready(function () {
 	const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 	const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
+
+	// переключатель фильтров
+	$('.filter-toggle').click(function (e) {
+		var target = $($(this).attr('target'));
+		target.slideToggle('fast');
+	});
+
 });
 /*
 	$('#content').magnificPopup({
